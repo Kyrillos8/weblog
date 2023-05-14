cb049607dac4138363df0407250adafffd6b69fa
Kyrillos8 <52589740+Kyrillos8@users.noreply.github.com>
2023-05-14 17:41:05 +0300

    code_modification_and_optimaization

--- django project/SmartBlog/NEWS/views.py
+++ django project/SmartBlog/NEWS/views.py
@@ -1,27 +1,23 @@
+# __ we dont need this cuz we are using microservise in the "djangoapi"
+
 from multiprocessing import context
 from urllib import response
 from django.shortcuts import render
-from django.http import JsonResponse, HttpResponse
-import json
 import requests
-# from newsapi import NewsApiClient
 
 # Create your views here.
 
-API_KEY = '595ecd51ec474a4aa58cb5ad59e05f93'
+API_KEY = '1df23db748374ef2a9de84c7f72d2288'
 
 def arti(request):
-    # data = response.json()
-    # country = requests.post('us')
+
     country = str(request.GET.get('country')).replace("None", "")
     category = str(request.GET.get('category')).replace("None", "")
 
     if category or country:
 
         response = requests.get(f'http://127.0.0.1:8081/test?country={country}&category={category}')
-        # response = requests.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=595ecd51ec474a4aa58cb5ad59e05f93')
         data = response.json()['articles']
-        # print(data)
 
     else:
         data = []
--- djangoapi/api/views.py
+++ djangoapi/api/views.py
@@ -4,46 +4,12 @@ from django.shortcuts import render
 from django.http import JsonResponse, HttpResponse
 import json
 import requests
-# from newsapi import NewsApiClient
 
 # Create your views here.
 
-API_KEY = '595ecd51ec474a4aa58cb5ad59e05f93'
+API_KEY = '1df23db748374ef2a9de84c7f72d2288'
 
 def arti(request):
-    # name = request.POST['u_name'] # u_name is the name of the input tag 
-	# age = request.POST['age'] 
-	# address = request.POST['address'] 
-    
-    # country = request.POST['country']
-    # newsapi = 'https://newsapi.org/v2/everything?q=tesla&from=2022-04-11&sortBy=publishedAt&apiKey=595ecd51ec474a4aa58cb5ad59e05f93'
-
-    # response = request.GET.get('https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=595ecd51ec474a4aa58cb5ad59e05f93')
-    # data = response.json()
-    # #country = 'us'
-    # #response = requests.get('https://newsapi.org/v2/top-headlines?country='+ country +'&apiKey=595ecd51ec474a4aa58cb5ad59e05f93')
-    # #data = json.loads(response.content)
-
-    # names = []
-    # for article in data['articles']:
-    #     # names.append(article['title'])
-    #     names.append(article['description'])
-    # context= {'names':names}
-
-    # l = data['articles']
-    # desc =[]
-    # news =[]
-    # img =[]
-    # url =[]
-  
-    # for i in range(len(l)):
-    #     f = l[i]
-    #     news.append(f['title'])
-    #     desc.append(f['description'])
-    #     img.append(f['urlToImage'])
-    #     url.append(f['url'])
-
-    # mylist = zip(news, desc, img, url)
     country = request.GET.get('country')
     category = request.GET.get('category')
     if country:
@@ -62,21 +28,4 @@ def arti(request):
     }
     print(articles)
     return JsonResponse(context)
-
-    # articles = []
-    # for article in data['articles']:
-    #     articles.append(article['author'])
-    # context={
-    #     'author':articles
-    # }
-
-    # response = requests.get("http://127.0.0.1:8081/test")
-    # print(response.text)
-    # data = json.loads(response.content)
-    # print(data['mylist'])
-
-    # context = {}
-    # return HttpResponse(response.text)
-    # return render(request,'NEWS/arti.html', context)
-
 
